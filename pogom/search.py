b8b2ab6f467925df5bd222131d1a484769eb467f
Oliver Heering <heering@riege.com>
2017-04-07 01:22:32 +0200

    Experimental 0.59.0 API

--- pogom/search.py
+++ pogom/search.py
@@ -459,7 +459,7 @@ def search_overseer_thread(args, beehive_workers, new_location_queue,
     account_queue = Queue()
     threadStatus = {}
     key_scheduler = None
-    api_version = '0.59.1'
+    api_version = '0.59.0'
     api_check_time = 0
 
     '''
