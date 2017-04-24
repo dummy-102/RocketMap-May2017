f688b2ece6655827428085078364ba8161284496
SkOODaT <skoodat@live.com>
2017-04-24 01:08:22 -0400

    My Name Is PokeHamilton!
    Start Of PokeHamilton Modifications...

--- static/js/statistics.js
+++ static/js/statistics.js
@@ -64,7 +64,7 @@ function addElement(pokemonId, name) {
 
     jQuery('<a/>', {
         id: 'link_' + pokemonId,
-        href: 'http://www.pokemon.com/us/pokedex/' + pokemonId,
+        href: 'http://www.pokehamilton.com/pokemon/' + pokemonId,
         target: '_blank',
         title: 'View in Pokedex',
         text: name
